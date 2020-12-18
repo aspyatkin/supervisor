bee905d69b7237b7767e33097d7e2cac3f31c404
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2020-12-18 13:14:43 +0400

    Replace python dependency with poise-python. Support Chef 14

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,8 +17,6 @@
 # limitations under the License.
 #
 
-include_recipe "python"
-
 # foodcritic FC023: we prefer not having the resource on non-smartos
 if platform_family?("smartos")
   package "py27-expat" do
@@ -26,7 +24,7 @@ if platform_family?("smartos")
   end
 end
 
-python_pip "supervisor" do
+python_package 'supervisor' do
   action :upgrade
   version node['supervisor']['version'] if node['supervisor']['version']
 end
